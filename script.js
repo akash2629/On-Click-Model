a2a6dc62fe7da1350ba8c597c7310a0a8f043d71
akash2629 <atiqulislamakash1@gmail.com>
2024-03-25 15:36:16 +0600

    completed Model

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup =document.querySelector('.popup');
+
+let openModel =() => {
+    popup.classList.add('popup_open')
+}
+
+let closeModel =() => {
+    popup.classList.remove('popup_open')
+}
